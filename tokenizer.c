9d091b65d4da1a36b8aeb27fbea9fc7419b10139
Daniel Tsioni <dtsioni@gmail.com>
2015-09-16 21:07:35 -0400

    Filled out fsm method for first state, the directory state, moved fsm method above tkgetnexttoken, and made the first state 1

--- tokenizer.c
+++ tokenizer.c
@@ -1,12 +1,5 @@
-/*
- * tokenizer.c
- */
 #include <stdio.h>
 #include <stdlib.h>
-
-/*
- * Tokenizer type.  You need to fill in the type as part of your implementation.
- */
 
 struct TokenizerT_ {
   char * inputStream;
@@ -15,20 +8,6 @@ struct TokenizerT_ {
 };
 
 typedef struct TokenizerT_ TokenizerT;
-
-/*
- * TKCreate creates a new TokenizerT object for a given token stream
- * (given as a string).
- *
- * TKCreate should copy the arguments so that it is not dependent on
- * them staying immutable after returning.  (In the future, this may change
- * to increase efficiency.)
- *
- * If the function succeeds, it returns a non-NULL TokenizerT.
- * Else it returns NULL.
- *
- * You need to fill in this function as part of your implementation.
- */
 
 TokenizerT *TKCreate( char * ts ) {
   TokenizerT* foo = (TokenizerT*)malloc(sizeof(TokenizerT));
@@ -60,15 +39,71 @@ void TKDestroy( TokenizerT * tk ) {
  *
  * You need to fill in this function as part of your implementation.
  */
+char *FSM( TokenizerT * tk ){
+  firstChar = *(tk->inputStream + tk->index);
+
+  while(true){
+    switch(tk->state){
+      case 1:
+        /* this is the first node in our fsm and acts as a sort of directory to the other main parts of our fsm */
+        if( ( firstChar >= 'a' && firstChar <= 'z' ) || ( firstChar >= 'A' && firstChar <= 'Z' ) ){
+          tk->state = 14;
+        }else if( firstChar == '0' ){
+          tk->state = 2;
+        }else if( firstChar >= '1' && firstChar <= '9' ){
+          tk->state = 8;
+        }else if( firstChar == '-' ){
+          tk->state = 15;
+        }else if( firstChar == '*' ){
+          tk->state = 19;
+        }else if( firstChar == '&' ){
+          tk->state = 21;
+        }else if( firstChar == '!' ){
+          tk->state = 24;
+        }else if( firstChar == '+' ){
+          tk->state = 26;
+        }else if( firstChar == '/' ){
+          tk->state = 29;
+        }else if( firstChar == '%' ){
+          tk->state = 31;
+        }else if( firstChar == '>' ){
+          tk->state = 33;
+        }else if( firstChar == '<' ){
+          tk->state = 37;
+        }else if( firstChar == '=' ){
+          tk->state = 41;
+        }else if( firstChar == '^' ){
+          tk->state = 43;
+        }else if( firstChar == '|' ){
+          tk->state = 45;
+        }else{
+          /* this is a character not recognized in any token */
+          return firstChar;
+        }
+        break;
+    }
+  }
+
+  return NULL;
+}
+
+/*
+ * TKGetNextToken returns the next token from the token stream as a
+ * character string.  Space for the returned token should be dynamically
+ * allocated.  The caller is responsible for freeing the space once it is
+ * no longer needed.
+ *
+ * If the function succeeds, it returns a C string (delimited by '\0')
+ * containing the token.  Else it returns 0.
+ *
+ * You need to fill in this function as part of your implementation.
+ */
 
 char *TKGetNextToken( TokenizerT * tk ) {
   char * token;
+  tk->state = 1;
   token = FSM(tk);
   return token;
-}
-
-char *FSM( TokenizerT * tk ){
-
 }
 
 /*
