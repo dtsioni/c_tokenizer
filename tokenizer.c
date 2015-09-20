5118710effb727790fa5c429afedcaf0e12c37a6
Daniel Tsioni <dtsioni@gmail.com>
2015-09-20 15:59:44 -0400

    fsm fully modeled

--- tokenizer.c
+++ tokenizer.c
@@ -35,7 +35,6 @@ void resultOutput( char * name, char * token ){
 char *FSM( TokenizerT * tk ){
   char currentChar;
   char nextChar;
-  int endOfToken;
   char * token;
   char * temp;
   char * type = "malformed";
@@ -43,7 +42,6 @@ char *FSM( TokenizerT * tk ){
   temp = (char *)malloc(sizeof(char)*2);
   token = (char *)malloc(sizeof(char));
 
-  endOfToken = 0;
   token[0] = '\0';
   // loop will run until our token is returned
 
@@ -51,11 +49,11 @@ char *FSM( TokenizerT * tk ){
   while(1){
 
     //printf("flag %d loop\n", __LINE__);
-    // find the state of our fsm that our tokenizer is on
+    //find the state of our fsm that our tokenizer is on
 
     currentChar = *(tk->inputStream + tk->index);
     nextChar = *(tk->inputStream + tk->index + 1);
-
+    //skip all white space to get to the next token
     if(currentChar == 0x20 || currentChar == 0x09 || ( currentChar >= 0x0a && currentChar <= 0x0d)){
       while(currentChar == 0x20 || currentChar == 0x09 || ( currentChar >= 0x0a && currentChar <= 0x0d)){
           tk->index = tk->index + 1;
@@ -87,36 +85,50 @@ char *FSM( TokenizerT * tk ){
       //printf("flag %d entered switch case 1\n", __LINE__);
         if( ( currentChar >= 'a' && currentChar <= 'z' ) || ( currentChar >= 'A' && currentChar <= 'Z' ) ){
           tk->state = 14;
+          type = "word";
         }else if( currentChar == '0' ){
           //printf("flag %d\n", __LINE__);
           tk->state = 2;
           type = "zero";
         }else if( currentChar >= '1' && currentChar <= '9' ){
           tk->state = 8;
+          type = "integer";
         }else if( currentChar == '-' ){
           tk->state = 15;
+          type = "minus/subtract";
         }else if( currentChar == '*' ){
           tk->state = 19;
+          type = "multiply/indirect";
         }else if( currentChar == '&' ){
           tk->state = 21;
+          type = "address/bitwise and";
         }else if( currentChar == '!' ){
           tk->state = 24;
+          type = "negate";
         }else if( currentChar == '+' ){
           tk->state = 26;
+          type = "add";
         }else if( currentChar == '/' ){
           tk->state = 29;
+          type = "divide";
         }else if( currentChar == '%' ){
           tk->state = 31;
+          type = "modulo";
         }else if( currentChar == '>' ){
           tk->state = 33;
+          type = "greater than";
         }else if( currentChar == '<' ){
           tk->state = 37;
+          type = "less than";
         }else if( currentChar == '=' ){
           tk->state = 41;
+          type = "assignment";
         }else if( currentChar == '^' ){
           tk->state = 43;
+          type = "bitwise exclusive or";
         }else if( currentChar == '|' ){
           tk->state = 45;
+          type = "bitwise or";
         }else{
           /* this is a character not recognized in any token */
           resultOutput(type, token);
@@ -124,10 +136,9 @@ char *FSM( TokenizerT * tk ){
         }
         break;
       case 2:
-      //printf("flag %d entered switch case 2\n", __LINE__);
-
         if( currentChar >= '0' && currentChar <= '7' ){
           tk->state = 3;
+          type = "octal";
         }else if( currentChar == 'X' || currentChar == 'x' ){
           tk->state = 4;
         }else if( currentChar == '.' ){
@@ -139,16 +150,311 @@ char *FSM( TokenizerT * tk ){
         }
         break;
       case 3:
-      //printf("flag %d entered switch case 3\n", __LINE__);
-        type = "octal";
         if( currentChar >= '0' && currentChar <= '7' ){
           tk->state = 3;
+          type = "octal";
         }else{
-          /* octal */
           resultOutput(type, token);
           return token;
         }
         break;
+      case 4:
+        if(( currentChar >= '0' && currentChar <= '9' ) || (currentChar >= 'A' && currentChar <= 'F') || (currentChar >= 'a' && currentChar <= 'f') ){
+          tk->state = 5;
+          type = "hexadecimal";
+        }else{
+          resultOutput(type, token);
+          return token;
+        }
+        break;
+      case 5:
+        if(( currentChar >= '0' && currentChar <= '9' ) || (currentChar >= 'A' && currentChar <= 'F') || (currentChar >= 'a' && currentChar <= 'f') ){
+          tk->state = 5;
+        }else{
+          resultOutput(type, token);
+          return token;
+        }
+        break;
+      case 6:
+        if( currentChar >= '0' && currentChar <= '9' ){
+          tk->state = 7;
+          type = "float";
+        }else{
+          resultOutput(type, token);
+          return token;
+        }
+        break;
+      case 7:
+        if( currentChar >= '0' && currentChar <= '9' ){
+          tk->state = 7;
+        }else if( currentChar == 'e' || currentChar == 'E' ){
+          tk->state = 11;
+        }else{
+          resultOutput(type, token);
+          return token;
+        }
+        break;
+      case 8:
+        if( currentChar >= '0' && currentChar <= '9' ){
+          tk->state = 8;
+          type = "integer";
+        }else if( currentChar == '.' ){
+          tk->state = 9;
+        }else if( currentChar == 'e' || currentChar == 'E' ){
+          tk->state = 11;
+        }else{
+          resultOutput(type, token);
+          return token;
+        }
+        break;
+      case 9:
+        if( currentChar >= '0' && currentChar <= '9' ){
+          tk->state = 10;
+          type = "float";
+        }else{
+          resultOutput(type, token);
+          return token;
+        }
+        break;
+      case 10:
+        if( currentChar >= '0' && currentChar <= '9' ){
+          tk->state = 10;
+          type = "float";
+        }else if( currentChar == 'e' || currentChar == 'E' ){
+          tk->state = 11;
+        }else{
+          resultOutput(type, token);
+          return token;
+        }
+        break;
+      case 11:
+        if( currentChar >= '0' && currentChar <= '9' ){
+          tk->state = 13;
+          type = "float";
+        }else if( currentChar == '+' || currentChar == '-' ){
+          tk->state = 12;
+        }else{
+          resultOutput(type, token);
+          return token;
+        }
+        break;
+      case 12:
+        if( currentChar >= '0' && currentChar <= '9' ){
+          tk->state = 13;
+          type = "float";
+        }else{
+          resultOutput(type, token);
+          return token;
+        }
+        break;
+      case 13:
+        if( currentChar >= '0' && currentChar <= '9' ){
+          tk->state = 13;
+          type = "float";
+        }else{
+          resultOutput(type, token);
+          return token;
+        }
+        break;
+      case 14:
+        if( ( currentChar >= 'a' && currentChar <= 'z' ) || ( currentChar >= 'A' && currentChar <= 'Z' ) || ( currentChar >= '0' && currentChar <= '9' )){
+          tk->state = 14;
+          type = "word";
+        }else{
+          resultOutput(type, token);
+          return token;
+        }
+        break;
+      case 15:
+        if( currentChar == '-' ){
+          tk->state = 16;
+          type = "decrement";
+        }else if( currentChar == '=' ){
+          tk->state = 17;
+          type = "minus equals";
+        }else if( currentChar == '>' ){
+          tk->state = 18;
+          type ="structure pointer";
+        }else{
+          resultOutput(type, token);
+          return token;
+        }
+        break;
+      case 16:
+      case 17:
+      case 18:
+        resultOutput(type, token);
+        return token;
+      case 19:
+        if( currentChar == '=' ){
+          tk->state = 20;
+          type = "times equals";
+        }else{
+          resultOutput(type, token);
+          return token;
+        }
+        break;
+      case 20:
+        resultOutput(type, token);
+        return token;
+      case 21:
+        if( currentChar == '&' ){
+          tk->state = 22;
+          type = "logical and";
+        }else if( currentChar == '=' ){
+          tk->state = 23;
+          type = "and equals";
+        }else{
+          resultOutput(type, token);
+          return token;
+        }
+        break;
+      case 22:
+      case 23:
+        resultOutput(type, token);
+        return token;
+      case 24:
+        if( currentChar == '=' ){
+          tk->state = 25;
+          type = "negate equals";
+        }else{
+          resultOutput(type, token);
+          return token;
+        }
+        break;
+      case 25:
+        resultOutput(type, token);
+        return token;
+      case 26:
+        if( currentChar == '+' ){
+          tk->state = 27;
+          type = "increment";
+        }else if( currentChar == '=' ){
+          tk->state = 28;
+          type = "add equals";
+        }else{
+          resultOutput(type, token);
+          return token;
+        }
+        break;
+      case 27:
+      case 28:
+        resultOutput(type, token);
+        return token;
+      case 29:
+        if( currentChar == '=' ){
+          tk->state = 30;
+          type = "divide equals";
+        }else{
+          resultOutput(type, token);
+          return token;
+        }
+        break;
+      case 30:
+        resultOutput(type, token);
+        return token;
+      case 31:
+        if( currentChar == '=' ){
+          tk->state = 32;
+          type = "mod equals";
+        }else{
+          resultOutput(type, token);
+          return token;
+        }
+        break;
+      case 32:
+        resultOutput(type, token);
+        return token;
+      case 33:
+        if( currentChar == '>' ){
+          tk->state = 34;
+          type = "shift right";
+        }else if( currentChar == '=' ){
+          tk->state = 36;
+          type = "greater than or equals";
+        }else{
+          resultOutput(type, token);
+          return token;
+        }
+        break;
+      case 34:
+        if( currentChar == '=' ){
+          tk->state = 35;
+          type = "shift right equals";
+        }else{
+          resultOutput(type, token);
+          return token;
+        }
+        break;
+      case 35:
+      case 36:
+        resultOutput(type, token);
+        return token;
+      case 37:
+        if( currentChar == '<' ){
+          tk->state = 38;
+          type = "shift left";
+        }else if( currentChar == '=' ){
+          tk->state = 40;
+          type = "less than or equals";
+        }else{
+          resultOutput(type, token);
+          return token;
+        }
+        break;
+      case 38:
+        if( currentChar == '=' ){
+          tk->state = 39;
+          type = "shift left equals";
+        }else{
+          resultOutput(type, token);
+          return token;
+        }
+        break;
+      case 39:
+      case 40:
+        resultOutput(type, token);
+        return token;
+      case 41:
+        if( currentChar == '=' ){
+          tk->state = 42;
+          type = "logical equals to";
+        }else{
+          resultOutput(type, token);
+          return token;
+        }
+        break;
+      case 42:
+        resultOutput(type, token);
+        return token;
+      case 43:
+        if( currentChar == '=' ){
+          tk->state = 44;
+          type = "bitwise exclusive or equals";
+        }else{
+          resultOutput(type, token);
+          return token;
+        }
+        break;
+      case 44:
+        resultOutput(type, token);
+        return token;
+      case 45:
+        if( currentChar == '|' ){
+          tk->state = 46;
+          type = "logical or";
+        }else if( currentChar == '=' ){
+          tk->state = 47;
+          type = "bitwise or equals";
+        }else{
+          resultOutput(type, token);
+          return token;
+        }
+        break;
+      case 46:
+      case 47:
+        resultOutput(type, token);
+        return token;
     }
     tk->index = tk->index + 1;
     // add char to tokenizer
@@ -159,10 +465,6 @@ char *FSM( TokenizerT * tk ){
       resultOutput(type, token);
       return token;
     }
-
-
-
-
   }
 
   return NULL;
